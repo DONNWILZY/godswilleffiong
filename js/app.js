dca54eb0c3db026d8d732d778887807af9d2387e
Godswill Effiong <askgodswill@gmail.com>
2022-07-11 22:33:02 +0100

    js added---- all set for deployment

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/*  ***********************typing animation */
+var typed = new Typed('.typing',{
+    strings: ["", "Web Designer", "Web Developer", "Digital Project Manager", "Cloud Engineer", "Google Certified Digital Marketer", "Certified Product Manager"],
+    typedSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
